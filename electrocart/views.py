2499f62c043ddfa00118381ff57f47e02513da38
YADAVALLI TEJAS <rockstarteja1049@gmail.com>
2025-06-26 17:07:29 +0530

    final

--- electrocart/views.py
+++ electrocart/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from category.models import Category
 from store.models import Product
+
+from store.models import ReviewRating
+
 def home(request):
-    products = Product.objects.all().filter(available=True)
+    products = Product.objects.all().filter(available=True).order_by('created_at')
     categories = Category.objects.all()
+    for product in products:
+        reviews = ReviewRating.objects.filter(product_id=product.id, status=True)
     context = {
         'products': products,
         'categories': categories,
+        'reviews': reviews,
     }
     return render(request, 'home.html', context)
